bee13cf3d92c9f8073f8ce665ebfb878e227bde6
lacey aguigam <laceyaguigam@yahoo.com>
2023-11-22 12:39:41 -0800

    added the acticity instructions to file

---
+++ script.js
@@ -0,0 +1,37 @@
+// Copy and paste the code below into your JavaScript file:
+
+
+// myGrades = [100, 100, 90, 73, 78, 94, 86];
+
+// myAverage = 0;
+
+// for (let i = 1; i <= myGrades.length; i++) {
+
+// myAverage += myGrades[i];
+
+// }
+
+// myAverage = myAverage / myGrades.length;
+
+// console.log("My grade average is " + myAverage);
+
+// The code above has bugs. Use your debugger to step through the code and find and fix any bugs.
+
+
+// What bugs did you find?
+
+
+// How did you fix them?
+
+
+// Write a function that accepts a number from 0-100.
+
+
+// Your function should reject input (throw an exception) if:
+
+// The value is not a number
+// Is less than 0 
+// Is greater than 100
+
+// Once you have that code working, create a new custom exception and throw that instead of a generic exception.
+// Once you have that working, write code that handles the exception and then call your function.
