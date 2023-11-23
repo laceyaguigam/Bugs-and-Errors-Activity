156edcd16adb6ae49c0a004a95ca2580b536507b
lacey aguigam <laceyaguigam@yahoo.com>
2023-11-22 21:25:38 -0800

    found the bugs and commented them out

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 // Copy and paste the code below into your JavaScript file:
 
+"use strict";
+  let myGrades = [100, 100, 90, 73, 78, 94, 86];
 
-// myGrades = [100, 100, 90, 73, 78, 94, 86];
+  let myAverage = 0;
 
-// myAverage = 0;
+for (let i = 1; i <= myGrades; i++) {
 
-// for (let i = 1; i <= myGrades.length; i++) {
+    myAverage += myGrades[i];
+}
 
-// myAverage += myGrades[i];
-
-// }
-
-// myAverage = myAverage / myGrades.length;
-
-// console.log("My grade average is " + myAverage);
+ myAverage = myAverage / myGrades;
+console.log("My grade average is " + myAverage);
 
 // The code above has bugs. Use your debugger to step through the code and find and fix any bugs.
 
 
 // What bugs did you find?
 
+    //function does not produce a grade average, it give NaN
+    //states myGrades and my Average is undefined 
 
 // How did you fix them?
-
+    //changed myGrades and myAverage into variables using let
+    //I removed myGrade.length and changed it to myGrades
 
 // Write a function that accepts a number from 0-100.
 
